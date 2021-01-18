ece36d6937d3047a8a2d252c993c1d3a9c20d03f
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2021-01-18 17:03:40 -0300

    função inserirFazenda criada

--- meurebanho/src/main/java/com/raphaelRoriz/meurebanho/controller/UsuarioController.java
+++ meurebanho/src/main/java/com/raphaelRoriz/meurebanho/controller/UsuarioController.java
@@ -1,8 +1,11 @@
 package com.raphaelRoriz.meurebanho.controller;
 
 
+import com.raphaelRoriz.meurebanho.Repository.FazendaRepository;
 import com.raphaelRoriz.meurebanho.Repository.UsuarioRepository;
+import com.raphaelRoriz.meurebanho.entity.FazendaEntity;
 import com.raphaelRoriz.meurebanho.entity.UsuarioEntity;
+import com.raphaelRoriz.meurebanho.service.FazendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -17,6 +21,9 @@ public class UsuarioController {
 
     @Autowired
     UsuarioRepository usuarioRepository;
+
+    @Autowired
+    FazendaService fazendaService;
 
     @GetMapping("/index")
     public ResponseEntity<List<UsuarioEntity>> listar(){
@@ -29,10 +36,21 @@ public class UsuarioController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(value = "/inserir")
-    public UsuarioEntity inserir(@Valid @RequestBody UsuarioEntity usuario){
+    @PostMapping(value = "/cadastrar")
+    public UsuarioEntity cadastrar(@Valid @RequestBody UsuarioEntity usuario){
         return usuarioRepository.save(usuario);
     }
-    
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/inserirFazenda/{idUsuario}")
+    public ResponseEntity<Object> inserirFazenda(@PathVariable Long idUsuario, @Valid @RequestBody FazendaEntity fazenda){
+        Optional<UsuarioEntity> usuario = usuarioRepository.findById(idUsuario);
+        if(usuario.isPresent()){
+            fazendaService.adicionarProprietario(fazenda,usuario);
+            return ResponseEntity.ok().body(fazenda);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
