232dd69e79463cd6986cd30c0f9bb261ae605eeb
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2021-01-18 14:16:48 -0300

    initial commit with spring project

---
+++ meurebanho/src/main/java/com/raphaelRoriz/meurebanho/MeuRebanhoApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelRoriz.meurebanho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuRebanhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuRebanhoApplication.class, args);
+	}
+
+}
---
+++ meurebanho/src/test/java/com/raphaelRoriz/meurebanho/MeuRebanhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelRoriz.meurebanho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuRebanhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
