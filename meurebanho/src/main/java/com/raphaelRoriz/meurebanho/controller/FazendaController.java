d298f100d33ec4846bf3afefc010244463ffdaef
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2021-01-18 20:43:56 -0300

    close #1

--- meurebanho/src/main/java/com/raphaelRoriz/meurebanho/controller/FazendaController.java
+++ meurebanho/src/main/java/com/raphaelRoriz/meurebanho/controller/FazendaController.java
@@ -1,8 +1,10 @@
 package com.raphaelRoriz.meurebanho.controller;
 
 import com.raphaelRoriz.meurebanho.Repository.FazendaRepository;
+import com.raphaelRoriz.meurebanho.entity.AnimalEntity;
 import com.raphaelRoriz.meurebanho.entity.FazendaEntity;
 import com.raphaelRoriz.meurebanho.entity.UsuarioEntity;
+import com.raphaelRoriz.meurebanho.service.AnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fazenda")
@@ -17,6 +20,9 @@ public class FazendaController {
 
     @Autowired
     FazendaRepository fazendaRepository;
+
+    @Autowired
+    AnimalService animalService;
 
     @GetMapping("/index")
     public ResponseEntity<List<FazendaEntity>> listar(){
@@ -33,4 +39,19 @@ public class FazendaController {
     public FazendaEntity inserir(@Valid @RequestBody FazendaEntity fazenda){
         return fazendaRepository.save(fazenda);
     }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/inserirAnimal/{idFazenda}")
+    public ResponseEntity<Object> inserirAnimal(@PathVariable Long idFazenda, @Valid @RequestBody AnimalEntity animal){
+        Optional<FazendaEntity> fazenda = fazendaRepository.findById(idFazenda);
+        if(fazenda.isPresent()){
+            animalService.adicionarFazenda(animal,fazenda);
+            return ResponseEntity.ok().body(animal);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
 }
